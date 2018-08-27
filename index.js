3e031e5987cd186d0483edf6eb99e21c5e98f26d
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-08-27 11:11:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // Code your solution in this file!
 
 function distanceFromHqInBlocks(street) {
- if (street ==) {return 1}
- else if (street > 43) {return street - 43}
+return street - 43
 }
 
 function distanceFromHqInFeet (someValue) {
