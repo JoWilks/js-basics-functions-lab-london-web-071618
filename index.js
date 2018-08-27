a8cbbac7f69d4d7ef98c33e203c3d9d8fe74e696
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-08-27 11:23:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,4 +17,6 @@ function calculatesFarePrice(start, destination) {
  let feet = Math.abs(start - destination) * 264
  if (feet <= 400) {return 0}
  else if (feet >= 400 && feet <= 2000) {return 0.02*(feet-400)}
+ else if (feet >=2000 && feet <=2500) {return 25}
+ else if (feet >=2500) {return 'cannot travel that far'}
 }
